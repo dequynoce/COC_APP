17bc94ecae082acebd74f29d6d48f56ddc4d76ba
dequynoce <cd.minz@gmail.com>
2024-07-04 21:23:07 +0700

    tạo project máy tech và thisinh

---
+++ COC_App/COC_Tech/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COC_Tech
+{
+    public partial class Form_Tech : Form
+    {
+        public Form_Tech()
+        {
+            InitializeComponent();
+        }
+    }
+}
